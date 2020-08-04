72185b1908f275bba49b4d584171781edf92909e
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-08-04 14:44:20 -0400

    working last objective

---
+++ main.js
@@ -0,0 +1,41 @@
+const span = document.querySelector('span.mess-with-me');
+span.style.fontSize = '3em';
+
+const pGraph = document.querySelector('p.mess-with-me');
+pGraph.style.backgroundColor = 'green';
+
+const removePic = document.querySelector('#hide-me');
+removePic.remove()
+
+const firstDino = document.getElementById('triceratops');
+
+firstDino.style.width = '324px';
+
+const turnOrange = function(){
+    span.style.color = 'orange'
+}
+
+span.addEventListener('click', turnOrange)
+
+const borderItUp = function(){
+    firstDino.style.border = ' 1px red solid'
+}
+
+firstDino.addEventListener('click', borderItUp);
+
+const featheredDino = document.getElementById('feathers')
+
+const transparent = function(){
+    featheredDino.style.opacity = '0.5'
+}
+featheredDino.addEventListener('click', transparent)
+
+const rowBackground = document.getElementById('row')
+
+
+const changeBackground = function(){
+    rowBackground.style.backgroundColor = 'red'
+}
+
+document.getElementById('toggle').addEventListener('click', changeBackground)
+
